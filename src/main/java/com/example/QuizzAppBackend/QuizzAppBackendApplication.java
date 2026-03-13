10fb931fa0a58cdb68b2f33fabdcb863b7d0aea9
manju1010 <manjcs081@rmkcet.ac.in>
2026-03-13 10:43:57 +0530

    first commit

---
+++ src/main/java/com/example/QuizzAppBackend/QuizzAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuizzAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizzAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizzAppBackendApplication.class, args);
+	}
+
+}
